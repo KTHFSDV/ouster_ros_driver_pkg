1c095e90c040b89ffa78d83081123abe6249adcf
kthfsdv-admin <kthfsdv@gmail.com>
2023-07-02 19:24:43 +0200

    changes docker networking, but doesnt work

--- docker_run.sh
+++ docker_run.sh
@@ -14,3 +14,5 @@ docker run \
     -v /xavier_ssd/docker_testing/$REPO_NAME/:/ws/src/$REPO_NAME/ \
     --name $REPO_NAME \
     $IMAGE_NAME:$IMAGE_TAG
+
+# LIDAR_IP_ADDRESS=$(avahi-resolve-host-name -4 os1-992005000477.local | awk '{print $2}') docker-compose up
