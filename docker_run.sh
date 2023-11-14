d57f13fc3751f293aebe7b4d5c9b594b2d3f77d9
PeoEneroth <sabolc.jut@kthformulastudent.se>
2023-11-14 22:38:23 +0100

    [DOCS] Added clarification about eth ports

--- docker_run.sh
+++ docker_run.sh
@@ -2,6 +2,6 @@
 # Docker run script
 
 export LIDAR_IP_ADDRESS=$(avahi-resolve-host-name -4 os1-992005000477.local | awk '{print $2}')
-export XAVIER_IP=$(ip addr show enp0s31f6 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)
+export XAVIER_IP=$(ip addr show enp0s31f6 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1) #change enp0s31f6 to lidar interface (its enp0s31f6 on lenovo)
 
 docker compose up
